b63be7a19f251f55f59b85c58df9c0c427bbec43
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2022-12-14 18:29:49 +0100

    ????

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,52 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+console.log(galleryEl);
+
+function creatImagsGalleryMarkup(){
+  return galleryItems.map(({preview, original, description})=> {
+    return 
+    `<div class="gallery__item">
+// <a class="gallery__link" href="${original}">
+//   <img
+//     class="gallery__image"
+//     src="${preview}"
+//     data-source="${original}"
+//     alt="${description}"
+//   />
+// </a>
+// </div>`;
+  }).join('');
+}
+// console.log()
+console.log(creatImagsGalleryMarkup());
+
+
+
+// const galleryEl = document.querySelector('.gallery');
+// // console.log(galleryEl);
+
+// const galleryMarkup = creatImagsGalleryMarkup(picture);
+// console.log(galleryMarkup);
+
+// galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// function creatImagsGalleryMarkup(galleryItems) {
+// return galleryItems.map(({preview, original, description}) => {
+//     return
+// `<div class="gallery__item">
+// <a class="gallery__link" href="${original}">
+//   <img
+//     class="gallery__image"
+//     src="${preview}"
+//     data-source="${original}"
+//     alt="${description}"
+//   />
+// </a>
+// </div>`;
+// }).join('');
+
+// }
+// console.log(creatImagsGalleryMarkup(picture));
